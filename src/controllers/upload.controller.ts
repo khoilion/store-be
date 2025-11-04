7b63b62fe3a6fdae8da5d3025eb65e6105acd797
khoi vu <zluffyz2002@gmail.com>
2025-11-04 16:04:07 +0700

    Update MinioService to provide presigned URL for uploading files with a specified expiry time.

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,25 +1,20 @@
-import { Elysia, t } from "elysia";
-import userService from "../services/UserService";
-import authMacro from "../macros/auth";
-import MinioService from "../services/MinioService";
+import {Elysia, t} from "elysia";
+import minioService from "../services/MinioService";
 
 const uploadController = new Elysia()
-  .group("/upload", group =>
-    group
-      .get("/presigned-url", async ({ query }) => {
-        const url = await MinioService.getInstance().getPreSignedUrl(query.objectName)
-        return {
-          url,
-          imageUrl: `https://khoi-upload.fcstoys.cloud/store/${query.objectName}`
-        }
-      }, {
-        query: t.Object({
-          objectName: t.String(),
-        }),
-        detail: {
-          tags: ["Upload"],
-        }
-      })
-  )
+    .group("/upload", group =>
+        group
+            .use(minioService)
+            .get("/presigned-url", async ({query, minioService}) => {
+                return await minioService.getPresignedUrl(query.objectName)
+            }, {
+                query: t.Object({
+                    objectName: t.String(),
+                }),
+                detail: {
+                    tags: ["Upload"],
+                }
+            })
+    )
 
 export default uploadController
